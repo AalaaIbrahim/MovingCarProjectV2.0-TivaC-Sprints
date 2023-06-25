8bacbb371064213a7ad7d68064b11d57534e4871
Hossam elwahsh <hacker2894@gmail.com>
2023-06-25 16:26:25 +0300

    update (main): flow

--- MovingCarV2/main.c
+++ MovingCarV2/main.c
@@ -1,20 +1,28 @@
 /**
- ** @file      main.c
- ** @brief     
- ** @author    
- ** @date      June 25, 2023
- ** @version   0.1
- */
+* @file    :   main.c
+* @author  :   Hossam Elwahsh - https://github.com/HossamElwahsh
+* @brief   :   Program File contains all main functions' implementation
+* @version :   0.1
+* @date    :   2023-06-25
+*
+* @copyright Copyright (c) 2023
+*/
 
-#include "app.h"
+#include "app_interface.h"
 
 int main(void)
 {
+    if(APP_OK == app_init())
+    {
+        app_start();
+    }
+    else
+    {
+        while(1)
+        {
 
-  while(1)
-  {
-
-  }
+        }
+    }
 
 	return 0;
 }
