24ac9e09f73bfc41f3dfaecc1d8a5e2ce21205de
AalaaIbrahim <118854389+AalaaIbrahim@users.noreply.github.com>
2023-06-25 08:27:28 +0300

    create: project

---
+++ MovingCarV2/HAL/btn/btn_interface.h
@@ -0,0 +1,4 @@
+#ifndef BTN_INTERFACE_H_
+#define BTN_INTERFACE_H_
+
+#endif 
---
+++ MovingCarV2/main.c
@@ -0,0 +1,20 @@
+/**
+ ** @file      main.c
+ ** @brief     
+ ** @author    
+ ** @date      June 25, 2023
+ ** @version   0.1
+ */
+
+#include "app.h"
+
+int main(void)
+{
+
+  while(1)
+  {
+
+  }
+
+	return 0;
+}
