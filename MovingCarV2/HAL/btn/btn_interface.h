95f358ab232cbe96bd46c132220250596c8373a5
AalaaIbrahim <118854389+AalaaIbrahim@users.noreply.github.com>
2023-06-26 16:00:58 +0300

    fix (btn): btn_init

--- MovingCarV2/HAL/btn/btn_interface.h
+++ MovingCarV2/HAL/btn/btn_interface.h
@@ -19,10 +19,11 @@ typedef enum {
 }enu_error_status_t_;
 
 /**
- *
- * @param en_a_gpio_port
- * @param en_a_gpio_pin
- * @return
+ * Function to initialize a push button on the given pin
+ * @param en_a_gpio_port : The port the button is connected on
+ * @param en_a_gpio_pin  : The pin the button is connected to
+ * @return ERROR_OK 		 : If the initialization is successful
+ * 				 BUTTON_NOK		 : If any of the arguments is invalid
  */
 enu_error_status_t_ button_init(en_gpio_port_t en_a_gpio_port, en_gpio_pin_t en_a_gpio_pin);
 
--- MovingCarV2/HAL/btn/btn_program.c
+++ MovingCarV2/HAL/btn/btn_program.c
@@ -1,64 +1,104 @@
-
 #include "btn_interface.h"
 #include "gpio_interface.h"
 
 void (*ptr_func_button_callback)(void) = NULL_PTR;
 
-st_gpio_cfg_t gl_str_button_config ;
-
-
 /**
- *	@brief			This function is used for initalizing button
- *	@param [in]		ptr_func						:	pointer to callback
- *	@return			ERROR_OK					    :	In case of successeion
- *  @return			PASSING_NULL_PTR				:	In case of passing null pointer
- *	@return			BUTTON_NOK        			    :	In case of wrong pin index
+ * Function to initialize a push button on the given pin
+ * @param en_a_gpio_port : The port the button is connected on
+ * @param en_a_gpio_pin  : The pin the button is connected to
+ * @return ERROR_OK 		 : If the initialization is successful
+ * 				 BUTTON_NOK		 : If any of the arguments is invalid
  */
-enu_error_status_t_ button_init(void(*ptr_func)(void))
+enu_error_status_t_ button_init(en_gpio_port_t en_a_gpio_port, en_gpio_pin_t en_a_gpio_pin)
 {
 
 	enu_error_status_t_ enu_error = ERROR_OK;
+	st_gpio_cfg_t st_lo_btn_pin_cfg;
 	
-	uint8_t_ uint8_pin_num = 0, uint8_port_num = 0;
-	uint8_pin_num = GPIO_PIN_4 % 10;
-	uint8_port_num = GPIO_PORT_F / 10;
-
-	if (uint8_port_num > 5 || uint8_pin_num > 7)
+	st_lo_btn_pin_cfg.port = en_a_gpio_port;
+	st_lo_btn_pin_cfg.pin  = en_a_gpio_pin;
+	st_lo_btn_pin_cfg.pin_cfg = INPUT_PULL_DOWN;
+	
+	enu_error = (enu_error_status_t_)gpio_pin_init(&st_lo_btn_pin_cfg);
+	
+	if(ERROR_OK != enu_error)
 	{
 		enu_error = BUTTON_NOK;
-		
 	}
 	else
 	{
-		if (((5 == uint8_port_num) && (uint8_pin_num > 4)) || ((4 == uint8_port_num) && (uint8_pin_num > 5)))
-		{
-			enu_error = BUTTON_NOK;
-		}
-		else
-		{ // button
-			gl_str_button_config.port = GPIO_PORT_F;
-			gl_str_button_config.pin = GPIO_PIN_4;
-
-			gl_str_button_config.pin_cfg = INPUT;
-			gl_str_button_config.current = PIN_CURRENT_2MA;
-//			gl_str_button_config.enu_pin_internal_attach = GPIO_PULLUP;
-			gpio_pin_init (&gl_str_button_config);
-//			gpio_pin_init(&gl_str_button_config);
-//			gpio_pin_enable_notification(SW_1);
-      gpio_enableInt(GPIO_PORT_F,GPIO_PIN_4);
-			if (NULL_PTR == ptr_func)
-			{
-				enu_error = PASSING_NULL_PTR;
-			}
-			else
-			{
-				gpio_setIntCallback(GPIO_PORT_F,GPIO_PIN_4, ptr_func);
-				ptr_func_button_callback = ptr_func;
-			}
-		}
+		/* Do Nothing */
 	}
+	
 	return enu_error;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  *	@brief			This function is used for getting button  state
